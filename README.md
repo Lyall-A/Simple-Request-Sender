eb1516b6739e27d32077c41f20357f7f92424f3d
Lyall <me@lyall.lol>
2025-07-02 11:35:44 +0100

    add more options + readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Simple Request Sender
 Sends requests on a interval, useful for keeping track of uptime
+
+## Options
+### `name` - string
+Name of request, optional
+### `url` - string
+URL for request, `{ping}` can be added to send the ping from previous request, required
+### `sendOnStart` - boolean
+If the request should be sent on startup, `false` by default
+### `interval` - integer
+How often a request should be sent, required
+### `waitForResponse` - boolean
+If the timeout for the next request should be created after response or not, `false` by default
+### `method` - string
+HTTP method, `GET` by default
+### `headers` - object
+HTTP headers, optional
+### `body` - any
+HTTP request body, optional
--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const requests = require("./requests.json");
 
 for (const request of requests) {
+    let ping = null;
+
     log(`Starting ${Math.floor(request.interval / 1000)}s interval`);
 
     if (request.sendOnStart) {
-        sendRequest();
+        sendRequest(); // Send request on start
     } else {
-        setTimeout(sendRequest, request.interval);
+        createTimeout(); // Wait for timeout
     }
 
     async function sendRequest() {
-        await fetch(request.url, {
+        const sendRequestDate = Date.now(); // For calculating ping
+
+        if (!request.waitForResponse) createTimeout(); // Create timeout before response
+
+        const formatOptions = {
+            ping
+        };
+
+        await fetch(formatString(request.url, formatOptions), {
             method: request.method,
             headers: request.headers,
             body: request.body
         }).then(res => {
-            log(`Server responded with ${res.status}${res.statusText ? ` - ${res.statusText}` : ""}`);
+            ping = Date.now() - sendRequestDate;
+            log(`Server responded with ${res.status}${res.statusText ? ` (${res.statusText})` : ""} in ${ping}ms`);
         }).catch(err => {
             log("Failed to send request:", err);
         });
 
-        setTimeout(sendRequest, request.interval);
+        if (request.waitForResponse) createTimeout(); // Create timeout after response
+    }
+    
+    function createTimeout() {
+        return setTimeout(sendRequest, request.interval);
     }
 
     function log(...msg) {
         console.log(`[${new Date().toLocaleString()}] [${request.name || request.url}]`, ...msg);
     }
+}
+
+function formatString(string, obj) {
+    return string
+        .replace(/{(.*?)}/g, (match, group) => group.split(".").reduce((acc, key) => acc && acc[key], obj) ?? "");
 }
